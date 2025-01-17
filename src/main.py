d52c6a5b060d0169c7dbc1a0117a3e88caedb0d8
stefan-sfarti <svsfarti@gmail.com>
2025-01-17 13:06:49 +0200

    CORS changes to allow frontend to interact with API

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from src.api.routes import question_routes
 from src.api.dependencies.dependencies import init_app_dependencies
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Or whatever your React app's origin is
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(question_routes.router)
 
 
